00c97c0d089e5af42a05ad9caa554d8bafcad0cf
Gergy <gergy2k07@gmail.com>
2024-12-10 00:00:29 +0300

    fix safety :)

--- rainblood_quotes/settings.py
+++ rainblood_quotes/settings.py
@@ -163,7 +163,8 @@ AUTHENTICATION_BACKENDS = (
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'projectrainbloodquotes@gmail.com'
-EMAIL_HOST_PASSWORD = 'rxja gorr cnaa kcrt'
+with open("psw") as rf:
+    EMAIL_HOST_PASSWORD = rf.read()
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'projectrainbloodquotes@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
